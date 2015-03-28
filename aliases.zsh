20c1fe8cc6064723b42822568243d25cc4e7a01c
Greg Mikeska <gmikeska@AUS-MAC-0988.attlocal.net>
2015-03-28 09:55:18 -0500

    initial commit, including test zsh script and the port of my current aliases.

---
+++ aliases.zsh
@@ -0,0 +1,10 @@
+alias g="git"
+alias gp="git push"
+alias gpull="git pull"
+alias gf="git fetch"
+alias gc="git checkout"
+alias gs="git status"
+alias gadd="git add -p"
+alias gr="git remote"
+
+echo "Aliases loaded."
